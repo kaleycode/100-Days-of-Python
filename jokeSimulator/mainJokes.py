0e8b3bcdd6ad65b1cd773b271a3859cc0b9671ca
kaleycode <readingkale@gmail.com>
2024-08-16 14:27:31 -0700

    Add files via upload

---
+++ jokeSimulator/mainJokes.py
@@ -0,0 +1,30 @@
+import os, json
+from flask import Flask, request
+import requests
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST"])
+def change():
+  page = ""
+  f = open("jokes.html")
+  page = f.read()
+  f.close()
+  result = requests.get("https://icanhazdadjoke.com/", headers={"Accept":"application/json"}) 
+  joke = result.json()
+  page = page.replace("{jokes}", joke["joke"])
+  return page
+
+@app.route('/')
+def index():
+  page = ""
+  f = open("jokes.html")
+  page = f.read()
+  f.close()
+  result = requests.get("https://icanhazdadjoke.com/", headers={"Accept":"application/json"}) 
+  joke = result.json()
+  page = page.replace("{jokes}", joke["joke"])
+  return page
+
+app.run(host='0.0.0.0', port=81)
