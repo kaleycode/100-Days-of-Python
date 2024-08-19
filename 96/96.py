53184332e2a68f77e29782aebb4e4f34704b5664
kaleycode <readingkale@gmail.com>
2024-08-19 08:55:12 -0700

    96.py

---
+++ 96/96.py
@@ -0,0 +1,19 @@
+#using beatiful soup to get the headings within a wikipedia page...
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://en.wikipedia.org/wiki/A._J._Raffles_(character)"
+
+response = requests.get(url)
+html = response.text
+
+soup = BeautifulSoup(html, "html.parser")
+
+text = ""
+
+article = soup.find_all("div", {"class": "mw-heading mw-heading3"})
+
+for i in article:
+  text += i.text
+  print(i.text)
