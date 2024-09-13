915d37ce35e13c06093bad21c7ebcbae72345e17
kaleycode <readingkale@gmail.com>
2024-09-13 13:30:32 -0700

    scraper.py

--- panda/scraper.py
+++ panda/scraper.py
@@ -5,4 +5,7 @@ pd.set_option("display.max_rows", None)
 scraper = pd.read_html("https://en.wikipedia.org/wiki/List_of_best-selling_albums")
 
 for index, table in enumerate(scraper):
-    print(scraper[1])
+    newScraper = scraper[1]
+df = pd.DataFrame(newScraper)
+newdf = df.drop("Ref(s)", axis='columns')
+print(newdf)
